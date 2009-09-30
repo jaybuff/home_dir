9ef12dd6607af6b7710cfd9353abffa05894018e
Jay Buffington <jaybuffington@gmail.com>
2009-09-30 12:34:48 -0700

    comments to notify_of_mail

--- bin/notify_of_mail.pl
+++ bin/notify_of_mail.pl
@@ -1,5 +1,12 @@
 #!/usr/bin/perl
 
+# this script is called via procmail (see my .procmailrc)
+# only works on a mac with growl installed
+# see http://www.growl.info/
+# also need to do this to install: 
+# sudo cpan -i Mac::Growl
+# sudo cpan -i Mail::Header
+
 use strict;
 use warnings;
 
@@ -20,6 +27,7 @@ my $subject = $head->get('Subject');
 chomp $from;
 chomp $subject;
 
+# don't notify me of the dns-ops mailing list
 if ( $prefix ne "[dns-ops] " ) { 
     PostNotification($app, $names[0], $from, $prefix . ' ' .  $subject);
 }
