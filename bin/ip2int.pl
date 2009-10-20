bf5682a14840af51b9c3a559df4104002df46fd6
Jay Buffington <jaybuffington@gmail.com>
2009-10-20 11:26:11 -0700

    fixed ip2int

--- bin/ip2int.pl
+++ bin/ip2int.pl
@@ -1,7 +1,9 @@
 #!/usr/bin/perl
+
 # Turn an IP into an integer for use with mysql server IDs (or whatever)
 
+$ip = shift @ARGV or die "usage: $0 <ip>\n";
 $exp = 3;
-map { $x += $_ * (2 ** (8 * $exp--)) } split(/\./, $ARGV[1]);
+map { $x += $_ * (2 ** (8 * $exp--)) } split(/\./, $ip);
 print $x
 
