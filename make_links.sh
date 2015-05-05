289c245b4ccc98fec861e394066cd31a65c48091
Jay Buffington <jaybuff@apple.com>
2015-05-05 15:57:31 -0700

    .tmux.conf

--- make_links.sh
+++ make_links.sh
@@ -1,5 +1,5 @@
-#!/bin/bash 
+#!/bin/bash
 
-for file in .bash_completion .bash_profile .bashrc bin .gitconfig .gitignore .git-prompt.sh .inputrc .mutt .muttrc .perltidyrc .pgp .procmailrc .pystartup .vagrant.d .vim .vimrc; do
+for file in .bash_completion .bash_profile .bashrc bin .gitconfig .gitignore .git-prompt.sh .inputrc .mutt .muttrc .perltidyrc .pgp .procmailrc .pystartup .vagrant.d .vim .vimrc .tmux.conf; do
 	echo  ln -s $(readlink -f $(dirname $0))/$file ~/$file
 done
